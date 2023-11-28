175a2e5da68092b8eafbb41c4f52306fa10faa9d
Kiran <145902144+KiranGadhavi@users.noreply.github.com>
2023-11-28 17:29:14 +0000

    add reactRouter full website

---
+++ src/main.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './components/Home.jsx'
+import About from './components/About.jsx'
+import Contact from './components/Contact.jsx'
+import User from './components/User.jsx'
+import Github, { githubInfoLoader } from './components/Github.jsx'
+
+// {const router = createBrowserRouter([
+//   {
+//   path: '/',
+//   element : <Layout/>,
+
+//   children:[
+//     {
+//       path: '',
+//       element : <Home/>,
+//   },
+//     { 
+//       path:'about',
+//       element: <About/>,
+//     }
+//   ]
+//   }
+// ])}
+const router = createBrowserRouter(
+createRoutesFromElements(
+  <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />} />
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
+      <Route path='user/:userid' element={<User />} />
+      <Route 
+      loader={githubInfoLoader}
+      path='github' 
+      element={<Github />}
+       />
+    </Route>
+)
+  )
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+  <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
